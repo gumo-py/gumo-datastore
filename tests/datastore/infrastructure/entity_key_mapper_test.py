6f8aba68f937d167e28235b8c7adfee65129ce49
Akira Yumiyama <yumiyama.akira@gmail.com>
2019-05-18 22:46:28 +0900

    Add test of EntityKey ID-style mapping

--- tests/datastore/infrastructure/entity_key_mapper_test.py
+++ tests/datastore/infrastructure/entity_key_mapper_test.py
@@ -34,3 +34,28 @@ def test_to_entity_key():
     assert entity_key.flat_pairs() == ['Book', 'name', 'BookComment', 'comment']
     assert entity_key.kind() == 'BookComment'
     assert entity_key.name() == 'comment'
+
+
+def test_to_datastore_id_key():
+    entity_key = EntityKeyFactory().build_from_pairs(pairs=[
+        ('Book', 12345),
+        ('BookComment', 67890),
+    ])
+    datastore_key = key_mapper.to_datastore_key(entity_key=entity_key)
+
+    assert isinstance(datastore_key, datastore.Key), 'must be an instance of datastore.Key'
+    assert isinstance(datastore_key.id, int)
+    assert datastore_key.name is None
+
+
+def test_to_entity_id_key():
+    datastore_key = datastore.Key(
+        'Book', 12345, 'BookComment', 67890,
+        project='test-project'
+    )
+    entity_key = key_mapper.to_entity_key(datastore_key=datastore_key)
+
+    assert isinstance(entity_key, EntityKey)
+    assert entity_key.flat_pairs() == ['Book', 12345, 'BookComment', 67890]
+    assert isinstance(entity_key.name(), int)
+    assert entity_key.name() == 67890
