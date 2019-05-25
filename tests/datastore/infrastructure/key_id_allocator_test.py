40d0318976475494909ae86147fbd96cde53b6f7
Akira Yumiyama <yumiyama.akira@gmail.com>
2019-05-25 21:11:45 +0900

    Refactoring tests structures

--- tests/datastore/infrastructure/key_id_allocator_test.py
+++ tests/datastore/infrastructure/key_id_allocator_test.py
@@ -5,21 +5,21 @@ from gumo.core.domain.entity_key import IncompleteKey
 from gumo.datastore.infrastructure.key_id_allocator import DatastoreKeyIDAllocator
 
 
-class TestEntityKeyGenerator:
-    _entity_key_generator = None
+class TestKeyIDAllocator:
+    _key_id_allocator = None
 
     @property
-    def entity_key_generator(self) -> KeyIDAllocator:
-        if self._entity_key_generator is None:
+    def key_id_allocator(self) -> KeyIDAllocator:
+        if self._key_id_allocator is None:
             injector.binder.bind(KeyIDAllocator, to=DatastoreKeyIDAllocator)
-            self._entity_key_generator = injector.get(KeyIDAllocator)
+            self._key_id_allocator = injector.get(KeyIDAllocator)
 
-        return self._entity_key_generator
+        return self._key_id_allocator
 
     def test_allocate_key(self):
         incomplete_key = EntityKeyFactory().build_incomplete_key(kind='Sample')
         assert isinstance(incomplete_key, IncompleteKey)
 
-        allocated_key = self.entity_key_generator.allocate(incomplete_key=incomplete_key)
+        allocated_key = self.key_id_allocator.allocate(incomplete_key=incomplete_key)
         assert allocated_key.kind() == 'Sample'
         assert isinstance(allocated_key.name(), int)
