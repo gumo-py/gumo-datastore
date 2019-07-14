99ceb223457ec902d9e0eab15816a3b59fb7c2bc
Akira Yumiyama <yumiyama.akira@gmail.com>
2019-07-14 21:01:56 +0900

    Update dependencies and add gumo-dev-server to sample app

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ import setuptools
 
 
 name = 'gumo-datastore'
-version = '0.1.0a1'
+version = '0.1.0a2'
 description = 'Gumo Datastore Library'
 dependencies = [
-    'gumo-core >= 0.1.0a6',
+    'gumo-core >= 0.1.0a7',
     'google-cloud-datastore >= 1.7.1',
 ]
 
