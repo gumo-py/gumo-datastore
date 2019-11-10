4ae89b15774d9d406619b3fd48c647b1b91866b1
Akira Yumiyama <yumiyama.akira@gmail.com>
2019-11-10 23:50:00 +0900

    Supports JsonProperty encode/decode

--- gumo/datastore/infrastructure/mapper/__init__.py
+++ gumo/datastore/infrastructure/mapper/__init__.py
@@ -1,5 +1,6 @@
 import datetime
-from typing import Optional
+import typing
+import json
 
 from gumo.core.injector import injector
 from gumo.datastore.infrastructure.entity_key_mapper import EntityKeyMapper
@@ -18,7 +19,7 @@ class DatastoreMapperMixin:
 
         return self._entity_key_mapper
 
-    def convert_datetime(self, t: datetime.datetime) -> Optional[datetime.datetime]:
+    def convert_datetime(self, t: datetime.datetime) -> typing.Optional[datetime.datetime]:
         if t is None:
             return
 
@@ -32,3 +33,9 @@ class DatastoreMapperMixin:
             microsecond=t.microsecond,
             tzinfo=datetime.timezone.utc,
         )
+
+    def loads_json_property(self, b: bytes) -> typing.Union[typing.List, typing.Dict]:
+        return json.loads(b.decode('utf-8'))
+
+    def dumps_json_property(self, doc: typing.Dict) -> bytes:
+        return json.dumps(doc).encode('utf-8')
