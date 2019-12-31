3af74f1d4fc962bd2ee46c9871f8b636fef9c016
Akira Yumiyama <yumiyama.akira@gmail.com>
2019-12-31 22:47:47 +0900

    Add _fetch_page() to DatastoreRepositoryMixin

--- gumo/datastore/infrastructure/repository/__init__.py
+++ gumo/datastore/infrastructure/repository/__init__.py
@@ -9,6 +9,7 @@ from gumo.datastore.infrastructure.configuration import DatastoreConfiguration
 from gumo.datastore.infrastructure.entity_key_mapper import EntityKeyMapper
 
 from google.cloud import datastore
+from google.cloud.datastore import Query
 
 
 class DatastoreRepositoryMixin:
@@ -44,6 +45,29 @@ class DatastoreRepositoryMixin:
 
     def to_datastore_key(self, entity_key: typing.Union[EntityKey, NoneKey, None]) -> typing.Optional[datastore.Key]:
         return self.entity_key_mapper.to_datastore_key(entity_key=entity_key)
+
+    def _fetch_page(
+        self, query: Query, limit: int, start_cursor: typing.Optional[bytes]
+    ) -> typing.Tuple[typing.List[DatastoreEntity], typing.Optional[str], bool]:
+        query_iterator = query.fetch(limit=limit, start_cursor=start_cursor)
+        pages = query_iterator.pages
+        page = next(pages)
+        entities = list(page)
+
+        next_cursor = (
+            query_iterator.next_page_token.decode("utf-8")
+            if query_iterator.next_page_token
+            else None
+        )
+        if next_cursor:
+            has_more = len(list(query.fetch(limit=1, start_cursor=next_cursor))) > 0
+        else:
+            has_more = False
+
+        if not has_more:
+            next_cursor = None
+
+        return entities, next_cursor, has_more
 
 
 @contextmanager
